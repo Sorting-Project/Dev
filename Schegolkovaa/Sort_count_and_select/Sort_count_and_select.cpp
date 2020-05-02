07321583f648f010188719ff09e327023497b979
Alex Biryukov <birukov20011@gmail.com>
2020-05-03 01:44:17 +0300

    3 contributors with full sortlist

---
+++ Schegolkovaa/Sort_count_and_select/Sort_count_and_select.cpp
@@ -0,0 +1,58 @@
+﻿// Sort_count_and_select.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include<iostream>
+
+using namespace std;
+
+template<typename T>
+void SelectionSort(T array[], const size_t size)
+{
+	for (size_t idx_i = 0; idx_i < size - 1; idx_i++)
+	{
+		size_t min_idx = idx_i;
+		for (size_t idx_j = idx_i + 1; idx_j < size; idx_j++)
+			if (array[idx_j] < array[min_idx])
+				min_idx = idx_j;
+		if (min_idx != idx_i)
+			swap(array[idx_i], array[min_idx]);
+	}
+}
+
+void CountingSort(int* arr, int n)
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+		if (arr[i] > k) k = arr[i];
+	int c[k] = { 0 };
+	for (int i = 0; i < n; i++)
+		c[arr[i]] = c[arr[i]] + 1;
+	int b = 0;
+	for (int i = 0; i < k; i++)
+		for (int j = 0; j < c[i]; j++)
+		{
+			arr[b] = i;
+			b = b + 1;
+		}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	CountingSort(a, n);
+	cout << "Сортировка подсчётом: ";
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
+	cout << endl;
+	a[n] = 0;
+	SelectionSort(a, n);
+	cout << "Сортировка выбором: ";
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
+	return 0;
+}
+
---
+++ Schegolkovaa/count_sort/count_sort.cpp
@@ -0,0 +1,38 @@
+﻿// count_sort.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include<iostream>
+
+using namespace std;
+
+void CountingSort(int* arr, int n)
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+		if (arr[i] > k) k = arr[i];
+	int c[k] = { 0 };
+	for (int i = 0; i < n; i++)
+		c[arr[i]] = c[arr[i]] + 1;
+
+	int b = 0;
+
+	for (int i = 0; i < k; i++)
+		for (int j = 0; j < c[i]; j++)
+		{
+			arr[b] = i;
+			b = b + 1;
+		}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	CountingSort(a, n);
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
+	return 0;
+}
