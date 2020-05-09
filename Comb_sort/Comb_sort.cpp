a067111947b399b68b5b2aee2bb6903e6a1aa253
LenaPetrenko <54915954+LenaPetrenko@users.noreply.github.com>
2020-05-09 19:24:52 +0300

    Update Comb_sort.cpp

--- Comb_sort/Comb_sort.cpp
+++ Comb_sort/Comb_sort.cpp
@@ -1,29 +1,42 @@
 ﻿// Comb_sort.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
 #include "pch.h"
 #include <iostream>
-
-void comb_sort(int *mas, int n)
+#include <vector>
+using namespace std;
+void comb_sort(vector<int> &vec, int size)
 {
 	int temp, k;
-	int s = n; // готовим начальный шаг
+	int s = size; // готовим начальный шаг
 	long long count = 0;
-	while (n > 1) {
+	while (size > 1) {
 		s /= 1.247f; // шаг на этом проходе.  На первом проходе получается примерно 80% от размера массива,
 										// и легкие элементы в хвосте переносятся в первые 20% 
 		if (s < 1) s = 1; // 0 быть не может, присвоим 1
 		k = 0; // нет перестановок
-		for (int i = 0; i + s < n; ++i) { // двигаемся, пока сравниваемый элемент (на s от текущего) в массиве
-			if (mas[i] / 10 > mas[i + s] / 10) {
-				temp = mas[i];
-				mas[i] = mas[i + s];
-				mas[i + s] = temp;
+		for (int i = 0; i + s < size; ++i) { // двигаемся, пока сравниваемый элемент (на s от текущего) в массиве
+			if (vec[i] / 10 > vec[i + s] / 10) {
+				temp = vec[i];
+				vec[i] = vec[i + s];
+				vec[i + s] = temp;
 				k = i;
 			}
 			++count;
 		}
 		if (s == 1) // шаг 1, как в обычном пузырьке. Включаем контроль	
-			n = k + 1;
+			size = k + 1;
 	}
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	cout << "введите числа, которые хотите отсортировать (для окончания ввода чисел, нажмите на любой знак, который не является числом)" << endl;
+	vector <int> vec;
+	int h;
+	while (cin >> h)
+		vec.push_back(h);
+	for (size_t i = 0; i < vec.size(); i++) cout << vec[i] << endl;
+	comb_sort(vec, vec.size());
+	for (auto i : vec)
+		cout << i << " ";
+	cout << endl;
 }
